5d57f1c6aa3cafdf893bcbed7eba41c5944d7272
MorganLevetti <113036290+MorganLevetti@users.noreply.github.com>
2023-03-30 10:53:58 +0200

    add user and route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/user/{id}', function (Request $request) {
+    return View('userPage');
 });
